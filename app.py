d82e3f6893fac0c156100d4bd6f48625a27dbca4
Michael Wilson <wilson.michael.c@gmail.com>
2019-05-16 13:42:37 -0400

    healthcheck

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Server Works!'
-  
+
+@app.route('/actuator/health')
+def health():
+    up = { 'status': 'up' }
+    return up
+
 @app.route('/scrape/<ticker>')
 def scrape_url(ticker):
     text = scraper.scrape_url(ticker, 'http://wilsoninformatics.com')
